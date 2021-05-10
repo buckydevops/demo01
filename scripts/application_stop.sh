45d508347b38e3c16fdcbe0ef5f0e457d32fa086
buckydevops <69618775+buckydevops@users.noreply.github.com>
2021-05-10 16:20:42 +0530

    Update application_stop.sh

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-##Stopping existing node servers
-##echo "Stopping any existing node servers"
-#pkill node
-# kill $(ps aux | grep '[n]ginx' | awk '{print $2}')
-/etc/init.d/nginx stop
-
+##Stopping existing nginx servers
+echo "Stopping any nginx server"
+kill $(ps aux | grep '[n]ginx' | awk '{print $2}')
